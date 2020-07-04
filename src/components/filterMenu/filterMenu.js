2072983172bc12146cf94bf1de325aa2bfea4127
JoaoVitor <jvmelop@gmail.com>
2020-07-03 22:53:43 -0300

    fix

--- src/components/filterMenu/filterMenu.js
+++ src/components/filterMenu/filterMenu.js
@@ -82,59 +82,14 @@ export default class FilterMenu extends Component{
     return (
       <Col xs={12} md={2} className="filter-menu-div">
         <Container fluid >
-        <Row id="filter-title-row" className="py-3">
-          <Col xs={12}>
-            <h1 id="filter-title-text">Filtros de Busca</h1>
-          </Col>
-        </Row>
-          <Row className="px-0">
-          {/**  TYPE FILTER */}
-          <Col xs={4} md={12} className="filter-div" >
-            <Row className="mx-0">
-            <Col xs={12} style={{fontSize: '16px'}}>
-              <p>Por Tipos</p>
-            </Col>
-            <Col xs={12} lg={5} className="px-0 mx-0">
-              <Dropdown>
-                <Dropdown.Toggle variant="success" id="dropdown-basic">
-                  {this.state.type_1 || "Nenhum"}
-                </Dropdown.Toggle>
-
-                <Dropdown.Menu>
-                  {this.state.typesNames.map( (name) => {
-                    return <Dropdown.Item 
-                              eventKey={name} 
-                              key={name+"_T1"}
-                              onSelect={(selected) => {this.handleSelect_Type(selected, 1)}}
-                              >{name}</Dropdown.Item>
-                  })}
-                </Dropdown.Menu>
-              </Dropdown>
-            </Col>
-            <Col xs={12} lg={5} className="px-0 mx-0">
-              <Dropdown>
-                <Dropdown.Toggle variant="success" id="dropdown-basic">
-                  {this.state.type_2 || "Nenhum"}
-                </Dropdown.Toggle>
-
-                <Dropdown.Menu>
-                {this.state.typesNames.map( (name) => {
-                    return <Dropdown.Item 
-                            eventKey={name} 
-                            key={name+"_T2"}
-                            onSelect={ (selected) => {this.handleSelect_Type(selected, 2)}}
-                            >{name}</Dropdown.Item>
-                  })}
-                </Dropdown.Menu>
-              </Dropdown>
-            </Col>
-            <Col xs={12} lg={1} className="px-0 ml-1" id="div-reset-types">
-              <Button onClick={this.resetSelectsT1T2} id="reset-button">×</Button>
+          <Row id="filter-title-row" className="py-3">
+            <Col xs={12}>
+              <h1 id="filter-title-text">Filtros de Busca</h1>
             </Col>
           </Row>
-          </Col>
+          <Row className="px-0">
 
-          {/**  REGION FILTER */}
+            {/**  NAME SEARCH */}
           <Col xs={4} md={12} className="filter-div" >
             <Row className="mx-0" >
             <Col xs={12} style={{fontSize: '16px'}}>
@@ -162,7 +117,82 @@ export default class FilterMenu extends Component{
             </Col>
             </Row>
           </Col>
-        
+
+            {/**  TYPE FILTER */}
+            <Col xs={4} md={12} className="filter-div" >
+              <Row className="mx-0">
+              <Col xs={12} style={{fontSize: '16px'}}>
+                <p>Por Tipos</p>
+              </Col>
+              <Col xs={12} lg={5} className="px-0 mx-0">
+                <Dropdown>
+                  <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    {this.state.type_1 || "Nenhum"}
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu>
+                    {this.state.typesNames.map( (name) => {
+                      return <Dropdown.Item 
+                                eventKey={name} 
+                                key={name+"_T1"}
+                                onSelect={(selected) => {this.handleSelect_Type(selected, 1)}}
+                                >{name}</Dropdown.Item>
+                    })}
+                  </Dropdown.Menu>
+                </Dropdown>
+              </Col>
+              <Col xs={12} lg={5} className="px-0 mx-0">
+                <Dropdown>
+                  <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    {this.state.type_2 || "Nenhum"}
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu>
+                  {this.state.typesNames.map( (name) => {
+                      return <Dropdown.Item 
+                              eventKey={name} 
+                              key={name+"_T2"}
+                              onSelect={ (selected) => {this.handleSelect_Type(selected, 2)}}
+                              >{name}</Dropdown.Item>
+                    })}
+                  </Dropdown.Menu>
+                </Dropdown>
+              </Col>
+              <Col xs={12} lg={1} className="px-0 ml-1" id="div-reset-types">
+                <Button onClick={this.resetSelectsT1T2} id="reset-button">×</Button>
+              </Col>
+            </Row>
+            </Col>
+
+            {/**  REGION FILTER */}
+            <Col xs={4} md={12} className="filter-div" >
+              <Row className="mx-0" >
+              <Col xs={12} style={{fontSize: '16px'}}>
+                <p>Por Região</p>
+              </Col>
+              <Col xs={12} lg={10} className="px-0 mx-0">
+                <Dropdown>
+                  <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    {this.state.region || "Nenhum"}
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu>
+                    {this.state.regionNames.map( (name) => {
+                      return <Dropdown.Item 
+                                eventKey={name} 
+                                key={name}
+                                onSelect={this.handleSelect_Region}
+                                >{name}</Dropdown.Item>
+                    })}
+                  </Dropdown.Menu>
+                </Dropdown>
+              </Col>
+              <Col xs={12} lg={1} className="px-0 ml-1">
+                <Button onClick={this.resetSelectRegion} id="reset-button">×</Button>
+              </Col>
+              </Row>
+            </Col>
+
           </Row>
         </Container>
       </Col>
